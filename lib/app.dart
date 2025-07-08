e6ded2cb58f0b03fb7435509eebdd14f41fae6d3
irfan <irfanahmadalvi19@gmail.com>
2025-07-08 13:26:01 +0600

    commit

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'ui/Screens/splash_screen.dart';
+
+class TaskManagerApp extends StatefulWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends State<TaskManagerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
